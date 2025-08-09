072ae16fce238612951847108849854e6a04a4ef
EduardoSegarraLedesma <eduardo.segarra.ledesma@alumnos.upm.es>
2025-08-09 23:42:34 +0200

    E2E Tests Firebase Bean fix

--- src/test/java/org/GoLifeAPI/e2e/UserTestIT.java
+++ src/test/java/org/GoLifeAPI/e2e/UserTestIT.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.*;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.ResultActions;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -12,6 +15,9 @@ public class UserTestIT extends CommonE2EMockIT {
 
     @AfterAll
     public void nextTestSetUp() throws Exception {
+        when(firebaseService.isAvailable()).thenReturn(true);
+        when(firebaseService.verifyBearerToken(any())).thenReturn("test-user");
+        when(firebaseService.deleteFirebaseUser(anyString())).thenReturn(true);
         String payload = "{\"nombre\":\"test-nombre\",\"apellidos\":\"test-apellidos\"}";
         mockMvc.perform(post("/api/usuarios")
                 .header("Authorization", "Bearer good.token")
